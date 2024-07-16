6c552b23a21a474921ba1495a7e9dc695d24889c
Sergey Korobeynikov <skoro@smn35.com>
2024-07-16 23:57:18 +0300

    API & Excel processing added

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,19 @@
-from typing import List
-import pandas as pd
+import os
 from pathlib import Path
+from typing import Any, Dict, List
+
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+from loader import load_user_settings
+
+load_dotenv(".env")
+
+file_path = "user_settings.json"
+data = load_user_settings(file_path)
+user_currencies = data.get("user_currencies", [])
+user_stocks = data.get("user_stocks", [])
 
 
 def convert_xlsx_to_list(file_name: str) -> List[dict]:
@@ -16,9 +29,51 @@ def convert_xlsx_to_list(file_name: str) -> List[dict]:
         print(type((e)))
     except Exception as e:
         print(f"Произошла ошибка при чтении файла '{file_name}': {str(e)}")
-
     return transactions
 
 
+def SP500(user_stocks: List[str]) -> dict:
+    stock_prices = {}
+    api_key = os.getenv("AV_API_KEY")
+    for stock in user_stocks:
+        url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock}&interval=1min&apikey={api_key}"
+        response = requests.get(url)
+        data = response.json()
+
+        if "Meta Data" in data:
+            try:
+                last_refreshed = data["Meta Data"]["3. Last Refreshed"]
+                stock_prices[stock] = data["Time Series (1min)"][last_refreshed]["1. open"]
+            except KeyError:
+                stock_prices[stock] = "N/A"
+                print(f"Ошибка получения данных для акции {stock}: Неверная структура данных")
+        else:
+            stock_prices[stock] = "N/A"
+            print(f"Ошибка получения данных для акции {stock}: {data}")
+
+    return stock_prices
+
+
+def exchange_rate(user_currencies: List[str]) -> Dict[str, Any]:
+    """Функция, возвращающая курс выбранныых валют к рублю"""
+    apikey = os.getenv("API_KEY")
+    results = {}
+    for currency in user_currencies:
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount=1"
+        headers = {"apikey": apikey}
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        if "result" in data:
+            results[currency] = round(data["result"], 2)
+        else:
+            results[currency] = {"rate_to_rub": "N/A", "error": data.get("error", "Unknown error")}
+    return results
+
+
 if __name__ == "__main__":
-    print(convert_xlsx_to_list('data/operations10.xlsx'))
+    # print(convert_xlsx_to_list('data/operations10.xlsx'))
+    print(SP500(user_stocks))
+    print(exchange_rate(user_currencies))
+    print("User Currencies:", user_currencies)
+    print("User Stocks:", user_stocks)
