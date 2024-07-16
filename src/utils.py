81575302500307b86241edc98caa4aabc6ef21f5
Sergey Korobeynikov <skoro@smn35.com>
2024-07-16 06:31:35 +0300

    convert_xlsx_to_list in utils.py fix

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,24 @@
 from typing import List
-
 import pandas as pd
+from pathlib import Path
 
 
 def convert_xlsx_to_list(file_name: str) -> List[dict]:
-    excel_df = pd.read_excel(file_name)
-    transactions = excel_df.to_dict(orient="records")
+    transactions = []
+    try:
+        if not Path(file_name).is_file():
+            raise FileNotFoundError(f"Файл '{file_name}' не найден.")
+
+        excel_df = pd.read_excel(file_name)
+        transactions = excel_df.to_dict(orient="records")
+
+    except ValueError as e:
+        print(type((e)))
+    except Exception as e:
+        print(f"Произошла ошибка при чтении файла '{file_name}': {str(e)}")
+
     return transactions
+
 
 if __name__ == "__main__":
     print(convert_xlsx_to_list('data/operations10.xlsx'))
