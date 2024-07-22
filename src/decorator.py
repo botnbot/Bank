9731cc1eb1a6bc346db4f9ff3ec05e59034b1525
Sergey Korobeynikov <skoro@smn35.com>
2024-07-22 19:34:57 +0300

    decorators.py added

---
+++ src/decorator.py
@@ -0,0 +1,19 @@
+import functools
+
+def log_result_to_file(file_name=None):
+    """
+    Декоратор, записывающий результат функции в файл.
+    Аргумент функции - путь для сохранения файла.
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if file_name is None:
+                # Если имя файла не передано, используем имя по умолчанию
+                file_name = f"{func.__name__}_result.txt"
+            with open(file_name, 'w') as file:
+                file.write(str(result))
+            return result
+        return wrapper
+    return decorator if file_name is None else decorator(file_name)
